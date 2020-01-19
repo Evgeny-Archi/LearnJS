8e5037e9c98ebbd7c879fc1355e7b7ef70721803
Evgeny <e.archibasov@gmail.com>
2020-01-19 23:48:47 +1000

    continue to learn

--- code.js
+++ code.js
@@ -1,6 +1,21 @@
 'use strict'
-function pageLoader() {
-  alert("Hello");
+
+window.onload = init;
+
+function init() {
+  let image = document.getElementsByTagName("img");
+  for (let i = 0; i < image.length; i++) {
+    image[i].onclick = showAnswer;
+  }
 }
 
-window.onload = pageLoader;
+function showAnswer(eventObj) {
+  let image = eventObj.target;
+  let name = image.id;
+  name = "img/" + name + ".jpg";
+  image.src = name;
+}
+
+
+
+
