79d634844a40b9ca4e8126f90466c4028175ace3
Evgeny Archibasov <e.archibasov@gmail.com>
2020-01-20 11:06:20 +1000

    continue learn

--- code.js
+++ code.js
@@ -19,3 +19,20 @@ function showAnswer(eventObj) {
 
 
 
+'use strict'
+
+window.onload = init;
+
+function init() {
+  let image = document.getElementsByTagName("img");
+  for (let i = 0; i < image.length; i++) {
+    image[i].onclick = showAnswer;
+  }
+}
+
+function showAnswer(eventObj) {
+  let image = eventObj.target;
+  let name = image.id;
+  name = "img/" + name + ".jpg";
+  image.src = name;
+}
