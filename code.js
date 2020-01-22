f4b53ff558f14c67a708ff2aa0eddf0a727faeb7
Evgeny Archibasov <e.archibasov@gmail.com>
2020-01-22 14:48:23 +1000

    End work with images game

--- code.js
+++ code.js
@@ -2,31 +2,13 @@
 
 window.onload = init;
 
+/*
+// Размытие при клике на картинку
 function init() {
   let image = document.getElementsByTagName("img");
   for (let i = 0; i < image.length; i++) {
     image[i].onclick = showAnswer;
-  }
-}
-
-function showAnswer(eventObj) {
-  let image = eventObj.target;
-  let name = image.id;
-  name = "img/" + name + ".jpg";
-  image.src = name;
-}
-
-
-
-
-'use strict'
-
-window.onload = init;
-
-function init() {
-  let image = document.getElementsByTagName("img");
-  for (let i = 0; i < image.length; i++) {
-    image[i].onclick = showAnswer;
+    image[i].onmousemove = showCoordinates;
   }
 }
 
@@ -42,4 +24,48 @@ function showAnswer(eventObj) {
     image.src = name;    
   }, 1000);
 }
+
+// Выводит координаты мышки на картинке
+function showCoordinates(eventObj) {
+  let coordinate = document.getElementById("coordinates");
+  coordinate.innerHTML = "X: " + eventObj.offsetX + ". Y: " + eventObj.offsetY;
+}
+*/
+
+// Размытие при наведении на картинку
+function init() {
+
+  const image = {
+    name: document.getElementsByTagName("img"),
+    showAnswer: (eventObj) => {
+      let path = eventObj.target;
+      let newSrc = path.id;
+      newSrc = "img/" + newSrc + ".jpg";
+      path.src = newSrc;
+    },
+    showOrigin: (eventObj) => {
+      let path = eventObj.target;
+      let originSrc = path.id;
+      originSrc = "img/" + originSrc + "blur.jpg";
+      path.src = originSrc;
+    },
+    coordinates: (eventObj) => {
+      let coordinate = document.getElementById("coordinates");
+      coordinate.innerHTML = "X: " + eventObj.offsetX + ". Y: " + eventObj.offsetY;
+    }
+  };
+
+  for (let i = 0; i < image.name.length; i++) {
+    image.name[i].onmouseover = image.showAnswer;
+    image.name[i].onmouseout = image.showOrigin;
+    image.name[i].onmousemove = image.coordinates;
+  }
+
+}
+
+
+
+
+
+
 
