194cdcd548ea85411c854c29f634ed201db6ea48
Evgeny Archibasov <e.archibasov@gmail.com>
2020-01-23 14:03:32 +1000

    end learn fly passenger fly

--- code.js
+++ code.js
@@ -1,22 +1,53 @@
 let passengers = [
   { name: "Igor", paid: true },
   { name: "John", paid: true },
-  { name: "Pete", paid: false },
-  { name: "Marina", paid: true }
+  { name: "Pete", paid: true },
+  { name: "Marina", paid: false },
+  { name: "Eva", paid: true }
 ];
 
-let blackList = ["John", "Marina"];
-
-
-let getPaidPassengers = (passengers) => {
-    let paidPassengers = [];
+// Фу-ция обработчик проверки пассажиров
+function checkPassengers(passengers, checkFunction) {
     for (let i = 0; i < passengers.length; i++) {
-        if (passengers[i].paid) {
-            paidPassengers.push(passengers[i]);
-            return paidPassengers;
-        } else {
-            continue;
+        if (checkFunction(passengers[i])) {
+            return false;
         }
     }
+    return true;
 }
+
+// Проверяет по черному списку. Если есть такой, возвращает true
+function checkBlackList(passenger) {
+    return (passenger.name == "John");
+}
+
+// Проверяет пассажиров на оплату. Если кто-то не оплатил, то возвращает true
+function checkPaidPassengers(passenger) {
+    return (!passenger.paid);
+}
+
+// Выводит список пассажиров
+function printPassengers(passenger) {
+    console.log("Имя пассажира: " + passenger.name);
+    if (passenger.paid) console.log("Оплата: Да");
+    else console.log("Оплата: Нет");
+    return false;
+}
+
+let canFly = checkPassengers(passengers, checkBlackList);
+if (!canFly) {
+    console.log("Can't fly. Black list");
+} else {
+    console.log("yes");
+}
+
+let allPaid = checkPassengers(passengers, checkPaidPassengers);
+if (!allPaid) {
+    console.log("Can't fly. Dont paid");
+} else {
+    console.log("yes");
+}
+
+checkPassengers(passengers, printPassengers);
+
 
