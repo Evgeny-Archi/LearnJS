c88a6bdd96aee464d91fa1e40cd05748f709aec9
Evgeny Archibasov <e.archibasov@gmail.com>
2020-01-24 14:31:31 +1000

    end learn fly passengers

--- code.js
+++ code.js
@@ -1,53 +1,10 @@
-let passengers = [
-  { name: "Igor", paid: true },
-  { name: "John", paid: true },
-  { name: "Pete", paid: true },
-  { name: "Marina", paid: false },
-  { name: "Eva", paid: true }
-];
+function fun(echo) {
+    console.log(echo);
+   };
 
-// Фу-ция обработчик проверки пассажиров
-function checkPassengers(passengers, checkFunction) {
-    for (let i = 0; i < passengers.length; i++) {
-        if (checkFunction(passengers[i])) {
-            return false;
-        }
-    }
-    return true;
+function echoMaker() {
+    return fun;
 }
 
-// Проверяет по черному списку. Если есть такой, возвращает true
-function checkBlackList(passenger) {
-    return (passenger.name == "John");
-}
-
-// Проверяет пассажиров на оплату. Если кто-то не оплатил, то возвращает true
-function checkPaidPassengers(passenger) {
-    return (!passenger.paid);
-}
-
-// Выводит список пассажиров
-function printPassengers(passenger) {
-    console.log("Имя пассажира: " + passenger.name);
-    if (passenger.paid) console.log("Оплата: Да");
-    else console.log("Оплата: Нет");
-    return false;
-}
-
-let canFly = checkPassengers(passengers, checkBlackList);
-if (!canFly) {
-    console.log("Can't fly. Black list");
-} else {
-    console.log("yes");
-}
-
-let allPaid = checkPassengers(passengers, checkPaidPassengers);
-if (!allPaid) {
-    console.log("Can't fly. Dont paid");
-} else {
-    console.log("yes");
-}
-
-checkPassengers(passengers, printPassengers);
-
-
+let bigFun = echoMaker();
+bigFun("Is there an echo?");
