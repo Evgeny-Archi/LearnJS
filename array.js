fa7ca4f64c908be8725e19a6ccde8d87a8683a00
Evgeny <e.archibasov@gmail.com>
2020-02-18 22:00:21 +1000

    Start todo list

--- array.js
+++ array.js
@@ -15,15 +15,5 @@ a.splice(3, 3, 'test') // Удаляет 3 элемента начиная с т
 */
 // shift() unshift() Добавляет и удаляет эл в начале массива
 
-fetch('http://api.openweathermap.org/data/2.5/weather?id=2013348&appid=8474fe60b649817c961cce9d55ad3ee0')
-    .then(function(resp) {return resp.json()})
-    .then(function(data) {
-        console.log(data);
-        document.querySelector(".city").textContent = data.name;
-        document.querySelector(".temp").innerHTML = (data.main.temp - 273.15) + " &deg;C";
-        document.querySelector(".weather").textContent = data.weather[0].description;
-        document.querySelector(".icon").innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png">`;
-    })
-    .catch(function() {
-        // catch any errors
-    });
+
+
